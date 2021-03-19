aaf617863ac64490654b4eae0a5fe2b90dc2b404
Sumanth-Balaji <78042259+Sumanth-Balaji@users.noreply.github.com>
2021-03-20 00:11:07 +0530

    sql statements for interface 4

---
+++ sql-queries/4.sql
@@ -0,0 +1,4 @@
+---Place Order button : 
+1)insert into my_order(customer_id,amount_paid,status) values (____from web-page____,0,"placing_order");
+2)insert into order_item(item_id,quantity) values (select item_id,quantity from cart where cart.customer_id=____from web-page____);
+---here order_id is generated in js file and inserted along with the above insert statements
