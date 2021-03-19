d33868782e6fe15983cb89fb3f5060a5f7d86021
Sumanth-Balaji <78042259+Sumanth-Balaji@users.noreply.github.com>
2021-03-20 00:19:48 +0530

    updated 2.sql

--- sql-queries/2.sql
+++ sql-queries/2.sql
@@ -1,7 +1,7 @@
 ---Menu button :
 select * from (select item.id,name,price,type,rating from item_tag,item_item_tag,item where 
               item.id=item_item_tag.item_id and item_item_tag.tag_id=item_tag.id) as B,
-              left outer join (select 1.0*sum(stars)/count(order_id),item_id as rating from rating group by item_id) 
+              left outer join (select 1.0*sum(stars)/count(order_id) as rating,item_id from rating group by item_id) 
               as A on A.item_id=B.id; 
 
 ---Cart button : 
