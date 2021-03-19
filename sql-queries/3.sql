558c485ae1164e27368872ab8b9f95dc78330801
Sumanth-Balaji <78042259+Sumanth-Balaji@users.noreply.github.com>
2021-03-20 00:08:56 +0530

    updated 3.sql

--- sql-queries/3.sql
+++ sql-queries/3.sql
@@ -1,6 +1,6 @@
 ---Search button : 
 select name,price,type,rating from item,item_tag,item_item_tag,
-                (select 1.0*sum(stars)/count(order_id),item_id as rating from rating group by item_id) as A where 
+                (select 1.0*sum(stars)/count(order_id) as rating,item_id from rating group by item_id) as A where 
                 item.id=item_item_tag.item_id and item_item_tag.tag_id=item_tag.id and A.item_id=item.id
                 and search_field=___input___;
 
