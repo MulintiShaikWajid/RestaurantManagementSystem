530708e98287fedc4b846eeb1247698e0099cac5
Sumanth-Balaji <78042259+Sumanth-Balaji@users.noreply.github.com>
2021-03-20 00:07:59 +0530

    sql statements for interface three

---
+++ sql-queries/3.sql
@@ -0,0 +1,8 @@
+---Search button : 
+select name,price,type,rating from item,item_tag,item_item_tag,
+                (select 1.0*sum(stars)/count(order_id),item_id as rating from rating group by item_id) as A where 
+                item.id=item_item_tag.item_id and item_item_tag.tag_id=item_tag.id and A.item_id=item.id
+                and search_field=___input___;
+
+---Add to cart button : 
+insert into cart(customer_id,item_id) values (___from web-page____);
