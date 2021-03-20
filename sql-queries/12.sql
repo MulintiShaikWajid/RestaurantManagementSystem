03c2a1202e416780fa7b59267e081c4ca564e1d1
Phanindra180 <55190993+Phanindra180@users.noreply.github.com>
2021-03-20 14:41:45 +0530

    Create 12.sql

---
+++ sql-queries/12.sql
@@ -0,0 +1,7 @@
+--adding to staff and person
+  insert into person (username,password,name) values (value1,value2,value3);
+  insert into staff values (value1,value2,value3,value4);
+  
+--deleting staff
+  delete from staff where id = given_id; 
+ 
