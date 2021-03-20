626a7be7e19022169eef25a7588fd9a6e1550ca5
Sumanth-Balaji <78042259+Sumanth-Balaji@users.noreply.github.com>
2021-03-20 13:17:20 +0530

    Update 03.sql

--- sql-queries/03.sql
+++ sql-queries/03.sql
@@ -1,8 +1,15 @@
----Search button : 
-select name,price,type,rating from item,item_tag,item_item_tag,
-                (select 1.0*sum(stars)/count(order_id) as rating,item_id from rating group by item_id) as A where 
-                item.id=item_item_tag.item_id and item_item_tag.tag_id=item_tag.id and A.item_id=item.id
-                and search_field=___input___;
+---display of items in menu :
+select * from (select item.id,name,price,type,rating from item_tag,item_item_tag,item where 
+              item.id=item_item_tag.item_id and item_item_tag.tag_id=item_tag.id) as B,
+              left outer join (select 1.0*sum(stars)/count(order_id) as rating,item_id from rating group by item_id) 
+              as A on A.item_id=B.id; 
+
+
+---deisplay of items in menu with search fields: 
+select * from (select item.id,name,price,type,rating from item_tag,item_item_tag,item where 
+              item.id=item_item_tag.item_id and item_item_tag.tag_id=item_tag.id) as B,
+              left outer join (select 1.0*sum(stars)/count(order_id) as rating,item_id from rating group by item_id) 
+              as A on A.item_id=B.id; search_field=___input___;
 
 ---Add to cart button : 
 insert into cart(customer_id,item_id,quantity) values (___from web-page____);
