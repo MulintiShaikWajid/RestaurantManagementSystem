af6661065920e428e84878717a1fc57897a29ed6
Mulinti Shaik Wajid <53876204+MulintiShaikWajid@users.noreply.github.com>
2021-03-20 21:10:26 +0530

    Update trig_one.sql

--- trig_one.sql
+++ trig_one.sql
@@ -1,9 +1,9 @@
-create function temp1() returns trigger as 
+create or replace function temp1() returns trigger as 
 $$
 begin
 insert into notifications
 select 'The following item has fallen below threshold'||new.name||'threshold is'||cast(new.threshold as text)||'quantity remaining is'||cast(new.quantity_remaining as text),now(),staff.id from staff where role_name='manager' ;
-return new
+return new;
 end;
 $$
 language plpgsql;
@@ -11,10 +11,9 @@ language plpgsql;
 
 
 
-create trigger low_inventory after update or insert of inventory
-referencing new row as nrow
+create trigger low_inventory after update or insert on inventory
 for each row
-when nrow.quantity_remaining<nrow.threshold
+when (new.quantity_remaining<new.threshold)
 execute procedure temp1();
 
 
