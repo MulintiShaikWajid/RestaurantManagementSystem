aa59e0184f22a67f4c44b2a55dd5ce8fd5d59486
Mulinti Shaik Wajid <53876204+MulintiShaikWajid@users.noreply.github.com>
2021-03-20 21:10:57 +0530

    Update trig_two.sql

--- trig_two.sql
+++ trig_two.sql
@@ -1,8 +1,8 @@
-create function temp2() returns trigger as 
+create or replace function temp2() returns trigger as 
 $$
 begin
 insert into notification values(cast((nrow.rcoins-orow.rcoins) as text)||' rcoins have been added to your account, the new total is '||cast(nrow.rcoins as text),now(),nrow.id);
-return new
+return new;
 end;
 $$
 language plpgsql;
@@ -11,10 +11,8 @@ language plpgsql;
 
 
 create trigger gift after update of rcoins on customer
-referencing new row as nrow
-referencing old row as orow
 for each row
-when nrow.rcoins>orow.rcoins
+when (new.rcoins>old.rcoins)
 execute procedure temp2();
 
 
