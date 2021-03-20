f50bca17a4aa279372d8f0ebee2c71fc68847989
Phanindra180 <55190993+Phanindra180@users.noreply.github.com>
2021-03-20 17:52:30 +0530

    Update 15.sql

--- sql-queries/15.sql
+++ sql-queries/15.sql
@@ -2,9 +2,9 @@
   update person set password = "",name="" ... where username = given_user_name;
 
 ---list 
-
----paid using Rcoins
-  update customer set rcoins = rcoins - (select sum(price*quantity) from order_item,item where  order_item.order_id = given_id and item.id = order_item.item_id) where id = given_cus_id;
-  update my_order set rcoins_used = (select sum(price*quantity) from order_item,item where  order_item.order_id = given_id and item.id = order_item.item_id) where id = given_id;
+  select username,price*quantity - rcoins_used from order_item,my_order,person,item where my_order.amount_paid is null and order_itme.order_id = given_id and item.id = order_item.item_id and person.id = my_order.customer_id ;
 ---paid using cash
   update my_order set amount_paid = (select sum(price*quantity) from order_item,item where  order_item.order_id = given_id and item.id = order_item.item_id) where id = given_id;
+
+---updating rcoins
+  update customer set rcoins =rcoins + 1 where id = given_cus_id;
