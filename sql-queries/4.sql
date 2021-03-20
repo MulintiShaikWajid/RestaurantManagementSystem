be8ad9fd334fe2a5e5af6a631953a796d5830974
Sumanth-Balaji <78042259+Sumanth-Balaji@users.noreply.github.com>
2021-03-20 09:17:57 +0530

    small change for remove button

--- sql-queries/4.sql
+++ sql-queries/4.sql
@@ -2,3 +2,6 @@
 1)insert into my_order(customer_id,amount_paid,status) values (____from web-page____,0,"placing_order");
 2)insert into order_item(item_id,quantity) values (select item_id,quantity from cart where cart.customer_id=____from web-page____);
 ---here order_id is generated in js file and inserted along with the above insert statements
+
+---Remove button
+delete from cart where customer_id=____from web-page____ and item_id=____from web-page_____;
