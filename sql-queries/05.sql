5eb1cebadc72c5dcbd334d83341fd93a25fa8541
Sumanth-Balaji <78042259+Sumanth-Balaji@users.noreply.github.com>
2021-03-20 13:31:01 +0530

    Update 05.sql

--- sql-queries/05.sql
+++ sql-queries/05.sql
@@ -2,3 +2,6 @@
 
 ---display of previous orders
 select * from my_order where my_order.customer_id=____from web-page____;
+
+---pay using rcoins button
+update customer set rcoins=(select rcoins from customer where id=____from web-page____)-___input___ where id=____from web-page____;
