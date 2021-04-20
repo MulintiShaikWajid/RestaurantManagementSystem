b720a94e8a10a2a03cc6ead38e99d7bfc1c5df1b
sumanthbalaji <sumanth9492135031@gmail.com>
2021-04-21 00:48:53 +0530

    update menu done

--- rms/controllers/updatemenu.js
+++ rms/controllers/updatemenu.js
@@ -50,13 +50,25 @@ exports.get_item = function(req,res,next){
                 }
                 else{
                     Updatemenu.get_item(req.params.id).then((item)=>{
-                        if(result.rowCount===0){
+                        if(item.rowCount===0){
                             res.render('one_item',{message:"Sorry, the url is invalid"});
                         }
+                        item_tags = []
+                        item_inven = []
+                        inven_qua={}
+                        for(var i=0;i<item.rowCount;i++){
+                            val = item.rows[i];
+                            if(item_tags.indexOf(val['tag_id'])===-1){
+                                item_tags.push(val['tag_id']);
+                            }
+                            if(item_inven.indexOf(val['inventory_id'])===-1){
+                                    item_inven.push(val['inventory_id']);
+                                    inven_qua[val['inventory_id']]=val['quantity_needed'];
+                                }
+                        }
                         Updatemenu.all_inven().then((inven)=>{
-                        console.log(inven);
                             Updatemenu.all_tags().then((tags)=>{
-                                res.render('one_item',{inven:inven.rows,tags:tags.rows,item:item.rows[0]});
+                                res.render('one_item',{item_tags:item_tags,item_inven:item_inven,inven_qua:inven_qua,inven:inven.rows,tags:tags.rows,item:item.rows[0]});
                             })
                         })
                     })
@@ -65,5 +77,42 @@ exports.get_item = function(req,res,next){
 }
 
 exports.update_item = function(req,res,next){
-    
+    if (! ("session_id" in req.signedCookies)){
+        res.cookie('redirect',req.url,{signed:true});
+        res.redirect('/login');
+        return;
+    }
+    else{
+        Person.get_details_from_session_id(req.signedCookies['session_id']).then(
+                (result)=>{
+                    Person.role(result.rows[0]['id']).then(
+                        (result2)=>{
+                            if(result2.rows[0]['role_name']!="manager"){
+                                res.redirect('/error');
+                            }
+                        })
+                    if(result.rowCount===0){
+                        res.cookie('redirect',req.url,{signed:true});
+                        res.redirect('/login');
+                    }
+                    else{
+                        // console.log(req.body);
+                        Updatemenu.update_item(req.params.id,req.body['name'],req.body['price']);
+                        Updatemenu.delete_inven(req.params.id);
+                        Updatemenu.delete_tag(req.params.id);
+                        for (var key in req.body){
+                            if(key.length<4){
+                                continue;
+                            }
+                            if(key.substr(0,4)=="tag_"){
+                                Updatemenu.insert_tag(req.params.id,req.body[key]);
+                            }
+                            if(key.substr(0,6)=="inven_" && req.body[key]!=''){
+                                Updatemenu.insert_inven(req.params.id,key.substr(6),req.body[key]);
+                            }
+                        }
+                        res.redirect('/updatemenu');
+                    }
+        })
+    }
 }
--- rms/models/updatemenu.js
+++ rms/models/updatemenu.js
@@ -5,15 +5,13 @@ module.exports = class Updatemenu{
     // }
     static items(){
         return pool.query("select * from item,\
-            (select item_item_tag.item_id,string_agg(item_tag.type,',') as tags from item_tag,item_item_tag \
+            (select item_item_tag.item_id,string_agg(item_tag.type,', ') as tags from item_tag,item_item_tag \
             where item_tag.id=item_item_tag.tag_id group by item_id) as A,\
-            (select item_id,string_agg(name_qua,',') as inventory from \
+            (select item_id,string_agg(name_qua,', ') as inventory from \
             (select item_id,concat(name,' ',quantity_needed,units) as name_qua from item_inventory,inventory where item_inventory.inventory_id=inventory.id) as B \
             group by item_id) as C \
             where item.id=A.item_id and item.id=C.item_id;");
     }
-    // 'INSERT INTO cart(user_id,item_id,quantity) VALUES ($1, $2, $3) ON CONFLICT (user_id,item_id) DO NOTHING;',[1,this.item_id,0]);
-    // 'INSERT INTO orders(user_id,item_id,quantity) SELECT user_id,item_id,quantity FROM cart ON CONFLICT (user_id,item_id) DO UPDATE SET quantity=orders.quantity+EXCLUDED.quantity;');
     static get_item(id){
         return pool.query("select item.id,item.name,item.price,inventory_id,quantity_needed,tag_id\
          from item,item_tag,item_item_tag,inventory,item_inventory \
@@ -25,5 +23,20 @@ module.exports = class Updatemenu{
     }
     static all_tags(){
         return pool.query("select * from item_tag;")
+    }
+    static delete_tag(item_id){
+        return pool.query("delete from item_item_tag where item_id=$1;",[item_id])
+    }
+    static insert_tag(item_id,tag_id){
+        return pool.query("insert into item_item_tag values ($1,$2);",[item_id,tag_id])
+    }
+    static delete_inven(item_id){
+        return pool.query("delete from item_inventory where item_id=$1;",[item_id])
+    }
+    static insert_inven(item_id,inven_id,quan){
+        return pool.query("insert into item_inventory values ($1,$2,$3);",[item_id,inven_id,quan]);
+    }
+    static update_item(item_id,name,price){
+        return pool.query("update item set name=$2 and price=$3 where id=$1;",[item_id,name,price]);
     }
     }
