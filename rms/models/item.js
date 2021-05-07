95c6cd2868168f7c3c50c31a3735e0d75708fe54
Mulinti Shaik Wajid <53876204+MulintiShaikWajid@users.noreply.github.com>
2021-05-07 22:39:45 +0530

    Update item.js

--- rms/models/item.js
+++ rms/models/item.js
@@ -2,7 +2,7 @@ const pool = require('../utils/database');
 
 module.exports = class Item{
     static get_all_items(){
-        return pool.query("select item.id,item.name,item.price,string_agg(item_tag.type,',') as tags from item,item_item_tag,item_tag where item.id=item_item_tag.item_id and item_item_tag.tag_id=item_tag.id group by item.id");
+        return pool.query("select round(avg(stars),2) as stars,id,name,price,tags from (select item.id,item.name,item.price,string_agg(item_tag.type,',') as tags from item,item_item_tag,item_tag where item.id=item_item_tag.item_id and item_item_tag.tag_id=item_tag.id group by item.id) as temp,rating where rating.item_id=temp.id group by temp.id,temp.name,temp.price,temp.tags");
     }
 
     static one_detail(id,user_id){
@@ -10,5 +10,8 @@ module.exports = class Item{
     }
     static update_quantity_of_item(user_id,item_id,new_quantity){
         return pool.query('insert into cart values($1,$2,$3) on conflict (customer_id,item_id) do update set quantity=$3 where cart.customer_id=$1 and cart.item_id=$2',[user_id,item_id,new_quantity])
+    }
+    static get_reviews(id){
+        return pool.query("select person.name as name, coalesce(stars,-1) as stars,coalesce(review,'Not reviewed') as review from rating,my_order,person where item_id=$1 and rating.order_id=my_order.id and my_order.customer_id=person.id",[id]);
     }
 }
