a2522985f79f46dde5145f86f8e9bc4f832528ec
gsaiabhishek <gsaiabhishek5@gmail.com>
2021-05-10 22:02:53 +0530

    minor changes in pug of customer

--- rms/utils/database.js
+++ rms/utils/database.js
@@ -1,14 +1,26 @@
 const { Pool } = require('pg');
-const { Client } = require('pg')
+//const { Client } = require('pg')
+
 const pool = new Pool({
-    user: 'wajid',     //your postgres username
+    user: 'postgres',     //your postgres username
     host: 'localhost', 
-    database: 'test', //your local database 
-    password: 'wajid', //your postgres user password
+    database: 'projectdb', //your local database 
+    password: 'postgres', //your postgres user password
     port: 5432, //your postgres running port
 });
-
 pool.connect();
+
+
+// const pool = new Pool({
+//     user: 'wajid',     //your postgres username
+//     host: 'localhost', 
+//     database: 'test', //your local database 
+//     password: 'wajid', //your postgres user password
+//     port: 5432, //your postgres running port
+// });
+// pool.connect();
+
+
 
 // const client = new Client({
 //     user: 'wajid',     //your postgres username
@@ -17,7 +29,8 @@ pool.connect();
 //     password: 'wajid', //your postgres user password
 //     port: 5432, //your postgres running port
 //   });
-
 // client.connect();
+
+
 
 module.exports = pool;
